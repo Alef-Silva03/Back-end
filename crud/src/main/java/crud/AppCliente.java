2a2f396c05db76ae223edfc8a21ccb232fff4c00
Alef-Silva03 <alefsilva1809@gmail.com>
2025-12-15 21:48:37 -0300

    Aula nova

---
+++ crud/src/main/java/crud/AppCliente.java
@@ -0,0 +1,95 @@
+package crud;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AppCliente {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClienteDAO clienteDAO = new ClienteDAO();
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Bem-vindo ao sistema de gerenciamento de Clientes!");
+
+		while (true) {
+			System.out.println("\nMenu:");
+			System.out.println("1. Adicionar Cliente");
+			System.out.println("2. Listar Clientes");
+			System.out.println("3. Atualizar Cliente");
+			System.out.println("4. Deletar Cliente");
+			System.out.println("5. Sair");
+
+			int escolha = scanner.nextInt();
+			scanner.nextLine(); // consumir nova linha
+
+			try {
+				switch (escolha) {
+				case 1:
+					System.out.print("Nome: ");
+					String nome = scanner.nextLine();
+
+					System.out.print("Email: ");
+					String email = scanner.nextLine();
+
+					System.out.print("Cpf: ");
+					String cpf = scanner.nextLine();
+
+					Cliente cliente = new Cliente();
+					cliente.setNome(nome);
+					cliente.setEmail(email);
+					cliente.setCpf(cpf);
+
+					clienteDAO.adicionarCliente(cliente);
+					System.out.println("Cliente adicionado!");
+					break;
+
+				case 2:
+					clienteDAO.listarClientes().forEach(u -> System.out
+							.println(u.getIdcliente() + " - " + u.getNome() + " - " + u.getEmail() + " - " + u.getCpf()));
+					break;
+
+				case 3:
+					System.out.print("Digite o ID do usuário a ser atualizado: ");
+					int idAtualizar = scanner.nextInt();
+					scanner.nextLine();
+
+					System.out.print("Novo Nome: ");
+					String novoNome = scanner.nextLine();
+
+					System.out.print("Novo Email: ");
+					String novoEmail = scanner.nextLine();
+
+					System.out.print("Nova Cpf: ");
+					String novoCpf = scanner.nextLine();
+
+					Cliente clienteAtualizar = new Cliente();
+					clienteAtualizar.setIdcliente(idAtualizar);
+					clienteAtualizar.setNome(novoNome);
+					clienteAtualizar.setEmail(novoEmail);
+					clienteAtualizar.setCpf(novoCpf);
+
+					clienteDAO.atualizarCliente(clienteAtualizar);
+					System.out.println("Cliente atualizado com sucesso!");
+					break;
+
+				case 4:
+					System.out.print("Digite o ID do usuário a ser deletado: ");
+					int idDeletar = scanner.nextInt();
+					clienteDAO.deletarCliente(idDeletar);
+					System.out.println("Cliente deletado com sucesso!");
+					break;
+
+				case 5:
+					System.out.println("Saindo...");
+					scanner.close();
+					return;
+
+				default:
+					System.out.println("Opção inválida!");
+				}
+			} catch (SQLException e) {
+				System.err.println("Erro: " + e.getMessage());
+			}
+		}
+	}
+}
---
+++ crud/src/main/java/crud/Cliente.java
@@ -0,0 +1,34 @@
+package crud;
+
+public class Cliente {
+	private int idcliente;
+	private String nome;
+	private String email;
+	private String cpf;
+	
+	public int getIdcliente() {
+		return idcliente;
+	}
+	public void setIdcliente(int idcliente) {
+		this.idcliente = idcliente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
